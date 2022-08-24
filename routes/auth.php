6941fa4246957f9d20497c9287515bb333c3994f
quancay <legend.cay@gmail.com>
2022-08-24 11:47:24 +0700

    setup router

---
+++ routes/auth.php
@@ -0,0 +1,45 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Users Register Route
+ */
+Route::prefix('/register')->group(function () {
+
+});
+
+/**
+ * Users login Route
+ */
+Route::prefix('/login')->group(function () {
+
+});
+
+
+
+/**
+ * Reset Password Email Route
+ */
+Route::prefix('/forgot-password')->group(function () {
+
+});
+
+
+/**
+ * Reset Password Route
+ */
+Route::prefix('/reset-password')->group(function () {
+
+});
+
+
+/**
+ * Change Password Route
+ */
+Route::prefix('/change-password')->group(function () {
+
+});
+
+
+
+
---
+++ routes/quiz.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/**
+ * Users Role Route
+ */
+
+Route::prefix('quiz')->group(function () {
+    
+});
